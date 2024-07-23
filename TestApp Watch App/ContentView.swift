c24e1ff8020bdb4200ec2424995813e8ee2cc89c
Matt <u7496235@anu.edu.au>
2024-07-23 17:53:34 +1000

    Initial Commit

---
+++ TestApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestApp Watch App
+//
+//  Created by Matt Bryant on 23/7/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestApp Watch App/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp Watch App
+//
+//  Created by Matt Bryant on 23/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
