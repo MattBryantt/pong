1a902edce3295c6b785b3925864902da0875a778
Matt <bryantmp53@gmail.com>
2024-08-02 19:33:10 +1000

    Implemented basic score.

--- TestApp Watch App/ContentView.swift
+++ TestApp Watch App/ContentView.swift
@@ -11,29 +11,41 @@ import Combine
 struct ContentView: View {
     @State private var ballPosition = CGPoint(x: 0, y: 0)
     @State private var ballVelocity = CGPoint(x: 1.5, y: 1.5)
-    @State private var paddlePosition: CGFloat = 0
+    @State private var paddlePosition: CGFloat = 0 // y position
     @State private var screenSize: CGSize = .zero
     @State private var scrollAmount = 0.0
+    
+    @State private var playerScore = 0
+    @State private var enemyScore = 0
+    
 
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 Rectangle()
+                    .frame(width:10, height: 50) // TODO: Fix doubled code
+                    .foregroundColor(.green)
+                    .position(x: 20,
+                              y: (geometry.size.height / 2))
+                Rectangle()
                     .frame(width: 10, height: 50)
                     .foregroundColor(.blue)
-                    .position(x: geometry.size.width - 40,
+                    .position(x: geometry.size.width - 20,
                               y: (geometry.size.height / 2) + scrollAmount)
                     .focusable(true)
                     .digitalCrownRotation($scrollAmount,
                                           from: -100.0,
-                                          through: 100.0,
-                                          by: 1.0)
-
+                                          through: 100.0, // TODO: Change to screen edge
+                                          by: 1.0,
+                                          sensitivity: .high)
                 Circle()
                     .frame(width: 20, height: 20)
                     .foregroundColor(.red)
                     .position(x: ballPosition.x,
                               y: ballPosition.y)
+                Text(verbatim: "Score: \(enemyScore) - \(playerScore)")
+                    .position(x: geometry.size.width / 2,
+                              y: 0)
             }
             .onAppear {
                 screenSize = geometry.size
@@ -63,20 +75,40 @@ struct ContentView: View {
         }
 
         // Bounce off paddle
-        if ballPosition.x >= screenSize.width - 50 {
-            let innerCalc = (screenSize.height / 2 + scrollAmount)
-            let tmp = abs(ballPosition.y - innerCalc)
-            if tmp <= 25 {
+//        if ballPosition.x >= screenSize.width - 30 { // TODO: Change to collision detection
+//            let innerCalc = (screenSize.height / 2 + scrollAmount)
+//            let tmp = abs(ballPosition.y - innerCalc)
+//            if tmp <= 25 {
+//                ballVelocity.x = -ballVelocity.x
+//            }
+//        }
+        
+        if ballVelocity.x >= 0 && ballPosition.x == screenSize.width - 30 {
+            print("Ball position: \(ballPosition.y)")
+            print("Paddle position: \(((screenSize.height / 2) + scrollAmount))")
+            if ballPosition.y - ((screenSize.height / 2) + scrollAmount) < 25 + 0.5 {
                 ballVelocity.x = -ballVelocity.x
             }
         }
-
-        // Reset if ball hits bottom
-//        if ballPosition.y >= screenSize.height {
-//            ballPosition = CGPoint(x: screenSize.width / 2, y: screenSize.height / 2)
-//            ballVelocity = CGPoint(x: 2, y: 2) // TODO: Fix doubled code
-//            print("Ball out of bounds")
-//        }
+        
+        
+        // Ball hits left
+        if ballPosition.x >= screenSize.width {
+            resetGame()
+            enemyScore += 1
+        }
+        
+        // Ball hits right
+        if ballPosition.x <= 0 {
+            resetGame()
+            playerScore += 1
+        }
+    }
+    
+    func resetGame() {
+        ballPosition = CGPoint(x: screenSize.width / 2,
+                               y: screenSize.height / 2)
+        ballVelocity = CGPoint(x: 1.5, y: 1.5) // TODO: Fix doubled code
     }
 }
 
