abb75de013474189a24dc9c52ad4eaa77f27f247
Matt <bryantmp53@gmail.com>
2024-08-07 19:52:23 +1000

    Added basic game over.

--- TestApp Watch App/ContentView.swift
+++ TestApp Watch App/ContentView.swift
@@ -17,13 +17,14 @@ struct ContentView: View {
     
     @State private var timer: Timer? = nil
     @State private var gamePaused = false
+    @State private var gameOpacity = 100.0
     
-    private var paddleHeight: CGFloat = 50
-    private var paddleWidth: CGFloat = 10
+    private var paddleHeight: CGFloat = 35
+    private var paddleWidth: CGFloat = 8
     
     private var paddleOffset: CGFloat = 20
     
-    private var ballSize: CGFloat = 20
+    private var ballSize: CGFloat = 15
     
     @State private var playerScore = 0
     @State private var enemyScore = 0
@@ -32,13 +33,18 @@ struct ContentView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
+                // Background colour (enables tap gesture)
+                Color.black.edgesIgnoringSafeArea(.all)
+                
                 Rectangle()
                     .frame(width: paddleWidth, height: paddleHeight) // TODO: Fix doubled code
                     .foregroundColor(.green)
                     .position(x: paddleOffset,
                               y: (geometry.size.height / 2) + AIamount)
+                    .opacity(gameOpacity)
+                
                 Rectangle()
-                    .frame(width: 10, height: 50)
+                    .frame(width: paddleWidth, height: paddleHeight)
                     .foregroundColor(.blue)
                     .position(x: geometry.size.width - paddleOffset,
                               y: (geometry.size.height / 2) + scrollAmount)
@@ -49,19 +55,31 @@ struct ContentView: View {
                                           by: 1.0,
                                           sensitivity: .low,
                                           isHapticFeedbackEnabled: false)
+                    .opacity(gameOpacity)
+                
                 Circle()
                     .frame(width: ballSize, height: ballSize)
                     .foregroundColor(.red)
                     .position(x: ballPosition.x,
                               y: ballPosition.y)
+                    .opacity(gameOpacity)
+                
                 Text(verbatim: "Score: \(enemyScore) - \(playerScore)")
                     .position(x: geometry.size.width / 2,
                               y: 0)
+                
+                if (gameOpacity < 100) {
+                    Text(verbatim: "Game Over!")
+                        .position(x: geometry.size.width / 2,
+                                  y: geometry.size.height / 2)
+                }
             }
+            
             .onAppear {
                 screenSize = geometry.size
                 startGame()
             }
+            
             .onTapGesture {
                 if (!gamePaused) {
                     print("Game Pause")
@@ -80,7 +98,6 @@ struct ContentView: View {
         timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
             updateBallPosition()
             updateAIPosition()
-            // print("x: \(ballVelocity.x), y: \(ballVelocity.y)")
         }
     }
 
@@ -96,8 +113,8 @@ struct ContentView: View {
         
         // Bounce off right
         if ballVelocity.x >= 0 &&
-            ballPosition.x >= screenSize.width - paddleOffset*1.8 &&
-            ballPosition.x <= screenSize.width - paddleOffset*1.5 {
+            ballPosition.x >= screenSize.width - (paddleOffset + paddleWidth*1.5) &&
+            ballPosition.x <= screenSize.width - (paddleOffset + paddleWidth) {
 
             if abs(ballPosition.y - ((screenSize.height / 2) + scrollAmount)) < paddleHeight*0.7 {
                 ballVelocity.x = -ballVelocity.x
@@ -106,8 +123,8 @@ struct ContentView: View {
         
         // Bounce off left
         if ballVelocity.x <= 0 &&
-            ballPosition.x <= paddleOffset*1.8 &&
-            ballPosition.x >= paddleOffset*1.5 {
+            ballPosition.x <= (paddleOffset + paddleWidth*1.5) &&
+            ballPosition.x >= (paddleOffset + paddleWidth) {
 
             if abs(ballPosition.y - ((screenSize.height / 2) + AIamount)) < paddleHeight*0.7 {
                 ballVelocity.x = -ballVelocity.x
@@ -128,19 +145,30 @@ struct ContentView: View {
     }
     
     func updateAIPosition() {
-        AIamount = ballPosition.x - (screenSize.height / 2)
+        AIamount = ballPosition.x - (screenSize.height / 2) // TODO: Change enemy paddle positioning
     }
     
     func resetBall() {
-        // timer?.invalidate() // TODO: Create pause on resetBall()
+        
         ballPosition = CGPoint(x: screenSize.width / 2,
                                y: screenSize.height / 2)
         let curX = abs(ballVelocity.x)
         let curY = abs(ballVelocity.y)
         ballVelocity = CGPoint(x: curX + 0.01,
                                y: curY + 0.01) // TODO: Fix doubled code
-        // TODO: Change to increasing velocity
         
+        // Pauses game before restart
+        timer?.invalidate()
+        
+        if (playerScore >= 7 || enemyScore >= 7) {
+            print("Game over")
+            gamePaused = true // TODO: Disable scroll on pause
+            gameOpacity = 50.0 // TODO: Fix opacity
+        } else {
+            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
+                startGame()
+            }
+        }
     }
 }
 
