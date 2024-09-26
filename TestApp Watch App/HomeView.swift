348035525e59798fbc3bd8f84a07f77100d7e5f0
Matt <bryantmp53@gmail.com>
2024-09-26 18:14:46 +1000

    Added home & options with difficulty feature.

---
+++ TestApp Watch App/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  TestApp Watch App
+//
+//  Created by Matt Bryant on 20/9/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ TestApp Watch App/OptionsView.swift
@@ -0,0 +1,18 @@
+//
+//  OptionsView.swift
+//  TestApp Watch App
+//
+//  Created by Matt Bryant on 21/9/2024.
+//
+
+import SwiftUI
+
+struct OptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptionsView()
+}
